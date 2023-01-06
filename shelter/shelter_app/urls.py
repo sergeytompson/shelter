03350a8abd8f5e48677cc6c1036983c7973fbfe1
sergeytompson <gfhfahfp@gmail.com>
2023-01-06 16:21:51 +0300

    04 - Поправленный

--- shelter/shelter_app/urls.py
+++ shelter/shelter_app/urls.py
@@ -6,15 +6,16 @@ from shelter import settings
 from .views import *
 
 urlpatterns = [
-    path('', ShelterListView.as_view(), name='home'),
-    path('shelters/<int:pk>', PetsListView.as_view(), name='shelter'),
-    path('pets/<int:pk>', PetDetailView.as_view(), name='pet'),
-    path('shelters/<int:pk>/create_pet', PetCreateView.as_view(), name='create pet'),
-    path('pets/<int:pk>/update', PetUpdateView.as_view(), name='update pet'),
-    path('pets/<int:pk>/delete', PetUpdateView.as_view(), name='delete pet'),
-    path('register/', ShelterUserRegisterView.as_view(), name='register'),
-    path('login/', ShelterUserLoginView.as_view(), name='login'),
-    path('logout/', ShelterUserLogoutView.as_view(), name='logout'),
+    # path('', ShelterListView.as_view(), name='home'),
+    # path('shelters/<int:pk>', ShelterDetailView.as_view(), name='shelter'),
+    # path('pets/<int:pk>', PetDetailView.as_view(), name='pet'),
+    path('', PetsListView.as_view(), name='pets'),
+    path('create_pet', PetCreateView.as_view(), name='create pet'),
+    # path('pets/<int:pk>/update', PetUpdateView.as_view(), name='update pet'),
+    # path('pets/<int:pk>/delete', PetUpdateView.as_view(), name='delete pet'),
+    # path('register/', ShelterUserRegisterView.as_view(), name='register'),
+    # path('login/', ShelterUserLoginView.as_view(), name='login'),
+    # path('logout/', ShelterUserLogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
